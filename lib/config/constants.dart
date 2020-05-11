d3afb289114e70661a52fe52917720f8bcc57cc3
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-05-11 12:28:42 +0530

    Initial commit

---
+++ lib/config/constants.dart
@@ -0,0 +1,11 @@
+class Constants{
+  static const BLOG_API = 'https://adityadroid-medium.herokuapp.com/@adityadroid';
+  static const MEDIUM_IMAGE_CDN = 'https://miro.medium.com/fit/c/700/300/';
+  static const BLOG_URL = 'http://www.medium.com/@adityadroid/';
+  static const PROFILE_MEDIUM = 'https://medium.com/@kumaraditya8894';
+  static const PROFILE_LINKEDIN = 'https://www.linkedin.com/in/kumar-aditya-77a2b4194/';
+  static const PROFILE_FACEBOOK = 'https://www.facebook.com/adityakomaar/';
+  static const PROFILE_GITHUB = 'https://github.com/nnhhiilliisstt';
+  static const PROFILE_TWITTER = 'https://twitter.com/Import_honesty';
+  static const PROFILE_INSTAGRAM = 'https://www.instagram.com/cosmicsuperclass/';
+}
