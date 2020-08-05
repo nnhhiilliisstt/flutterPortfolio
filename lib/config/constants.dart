5d9d77859b701fe2c43d12b4abb9a85c8830415d
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-08-05 19:18:55 +0530

    addi

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,11 +1,14 @@
-class Constants{
-  static const BLOG_API = 'https://adityadroid-medium.herokuapp.com/@adityadroid';
+class Constants {
+  static const BLOG_API =
+      'https://adityadroid-medium.herokuapp.com/@kumaraditya8894';
   static const MEDIUM_IMAGE_CDN = 'https://miro.medium.com/fit/c/700/300/';
   static const BLOG_URL = 'https://medium.com/@kumaraditya8894';
   static const PROFILE_MEDIUM = 'https://medium.com/@kumaraditya8894';
-  static const PROFILE_LINKEDIN = 'https://www.linkedin.com/in/kumar-aditya-77a2b4194/';
+  static const PROFILE_LINKEDIN =
+      'https://www.linkedin.com/in/kumar-aditya-77a2b4194/';
   static const PROFILE_FACEBOOK = 'https://www.facebook.com/adityakomaar/';
   static const PROFILE_GITHUB = 'https://github.com/nnhhiilliisstt';
   static const PROFILE_TWITTER = 'https://twitter.com/Import_honesty';
-  static const PROFILE_INSTAGRAM = 'https://www.instagram.com/cosmicsuperclass/';
+  static const PROFILE_INSTAGRAM =
+      'https://www.instagram.com/cosmicsuperclass/';
 }
